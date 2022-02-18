89a2824747beb902c85200111aba7311f8dcd758
yzzeee <goooooodgood@gmail.com>
2022-02-18 21:52:50 +0900

    Create Pattern : Singleton - Implement the safest way to prevent breaking singleton using enum
    - The enum implements serializable.
    - The enum is blocked in reflection.
    - Disadvantage is that lazy initialization is not possible.

--- src/main/java/com/create/singleton/Main.java
+++ src/main/java/com/create/singleton/Main.java
@@ -1,24 +1,17 @@
 package com.create.singleton;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings settings1 = Settings.getInstance();
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Settings settings1 = Settings.INSTANCE;
         Settings settings2 = null;
 
-        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
-            out.writeObject(settings1);
-        }
-
-        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-            settings2 = (Settings) in.readObject();
+        Constructor<?>[] declaredConstructors = Settings.class.getDeclaredConstructors();
+        for (Constructor<?> constructor : declaredConstructors) {
+            constructor.setAccessible(true);
+            settings2 = (Settings) constructor.newInstance("INSTANCE");
         }
 
         System.out.println(settings1 == settings2);
