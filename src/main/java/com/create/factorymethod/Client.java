e280d1d0033c5f69723a0f3128676e7a3bcb53a0
yzzeee <goooooodgood@gmail.com>
2022-02-18 22:00:59 +0900

    Create Pattern : Factory Method - Minimize Clinet code changes

--- src/main/java/com/create/factorymethod/Client.java
+++ src/main/java/com/create/factorymethod/Client.java
@@ -2,10 +2,12 @@ package com.create.factorymethod;
 
 public class Client {
     public static void main(String[] args) {
-        Ship whiteship = new WhiteShipFactory().orderShip("Whiteship", "white@example.com");
-        System.out.println(whiteship);
+        Client client = new Client();
+        client.print(new WhiteShipFactory(), "Whiteship", "white@example.com");
+        client.print(new BlackShipFactory(), "Blackship", "black@example.com");
+    }
 
-        Ship blackship = new BlackShipFactory().orderShip("Blackship", "black@example.com");
-        System.out.println(blackship);
+    public static void print(ShipFactory shipFactory, String name, String email) {
+        System.out.println(shipFactory.orderShip(name, email));
     }
 }
