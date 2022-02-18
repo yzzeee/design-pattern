4dfe8737c533741e271dd72c38e56b44c2e9268e
yzzeee <goooooodgood@gmail.com>
2022-02-18 21:47:56 +0900

    Create Pattern : Singleton - Break singleton using reflection

--- src/main/java/com/create/singleton/Main.java
+++ src/main/java/com/create/singleton/Main.java
@@ -1,8 +1,16 @@
 package com.create.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args) {
-        Settings settings = Settings.getInstance();
-        System.out.println(settings == Settings.getInstance());
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Settings settings1 = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();
+
+        System.out.println(settings1 == settings2);
     }
 }
